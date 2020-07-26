1a8bace06a18a0f7a39f75f4296fb40301626bf7
obeycats18@gmail.com <hA9FrW*R3^Y2>
2020-07-26 13:55:45 +0300

    Updates

---
+++ precache-manifest.a67fbbd17a50d8a9466a29eab51a1149.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d848a05d54e274d698c1a0f15de42e",
+    "url": "/db2_react_test/index.html"
+  },
+  {
+    "revision": "93670fbbb98e8c92b0b8",
+    "url": "/db2_react_test/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "13485ec61273e320df44",
+    "url": "/db2_react_test/static/css/main.7084384f.chunk.css"
+  },
+  {
+    "revision": "93670fbbb98e8c92b0b8",
+    "url": "/db2_react_test/static/js/2.2c4b8595.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/db2_react_test/static/js/2.2c4b8595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13485ec61273e320df44",
+    "url": "/db2_react_test/static/js/main.9715e59c.chunk.js"
+  },
+  {
+    "revision": "56ae132e75afe6083590",
+    "url": "/db2_react_test/static/js/runtime-main.1934cc24.js"
+  },
+  {
+    "revision": "455f51ee477534ebedc1ab5d36cff948",
+    "url": "/db2_react_test/static/media/logo.455f51ee.svg"
+  }
+]);
